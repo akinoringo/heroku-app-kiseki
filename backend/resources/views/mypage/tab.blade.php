4442de005b90dad9794fc85c8beabf8032176df0
akinoringo <wbraver@outlook.jp>
2021-05-14 16:01:39 +0900

    マイページのUIを修正

--- backend/resources/views/mypage/tab.blade.php
+++ backend/resources/views/mypage/tab.blade.php
@@ -4,36 +4,35 @@
   <li class="nav-item text-center">
     <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
       {{$goals[0]->title}}<br>
-      {{$goals[0]->efforts_time}}/{{$goals[0]->goal_time}} [時間]
+      {{-- {{$goals[0]->efforts_time}}/{{$goals[0]->goal_time}} [時間] --}}
     </a>
-    <a class="small" href="{{route('goals.show', ['goal'=>$goals[0]])}}">目標詳細</a> 
+
   </li>
   @endif
   @if (isset($goals[1]))
   <li class="nav-item text-center">
     <a class="nav-link" id="pills-second-tab" data-toggle="pill" href="#pills-second" role="tab" aria-controls="pills-second" aria-selected="false">
       {{$goals[1]->title}}<br>
-      {{$goals[1]->efforts_time}}/{{$goals[1]->goal_time}} [時間]
+      {{-- {{$goals[1]->efforts_time}}/{{$goals[1]->goal_time}} [時間] --}}
     </a>
-    <a class="small" href="{{route('goals.show', ['goal'=>$goals[1]])}}">目標詳細</a>
   </li>
   @endif
   @if (isset($goals[2]))  
   <li class="nav-item text-center">
     <a class="nav-link" id="pills-third-tab" data-toggle="pill" href="#pills-third" role="tab" aria-controls="pills-third" aria-selected="false">
       {{$goals[2]->title}}<br>
-      {{$goals[2]->efforts_time}}/{{$goals[2]->goal_time}} [時間]
+      {{-- {{$goals[2]->efforts_time}}/{{$goals[2]->goal_time}} [時間] --}}
     </a>
-    <a class="small" href="{{route('goals.show', ['goal'=>$goals[2]])}}">目標詳細</a>
   </li>
   @endif
 </ul>
 
-@include('mypage.search')
-
 <div class="tab-content" id="pills-tabContent">
   @if(isset($efforts[0]))
   <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+    <a href="{{route('goals.show', ['goal'=>$goals[0]])}}">
+      <i class="fas fa-chevron-circle-right mr-1"></i>目標：{{ $goals[0]->title }}の詳細はこちら
+    </a>     
     @foreach($efforts[0] as $effort)
     @include('efforts.card')
     @endforeach 
@@ -42,6 +41,9 @@
   @endif 
   @if(isset($efforts[1]))
   <div class="tab-pane fade" id="pills-second" role="tabpanel" aria-labelledby="pills-second-tab">
+    <a href="{{route('goals.show', ['goal'=>$goals[1]])}}">
+      <i class="fas fa-chevron-circle-right mr-1"></i>目標：{{ $goals[1]->title }}の詳細はこちら
+    </a>    
     @foreach($efforts[1] as $effort)
     @include('efforts.card')
     @endforeach
@@ -50,6 +52,9 @@
   @endif 
   @if(isset($efforts[2])) 
   <div class="tab-pane fade" id="pills-third" role="tabpanel" aria-labelledby="pills-third-tab">
+    <a href="{{route('goals.show', ['goal'=>$goals[2]])}}">
+      <i class="fas fa-chevron-circle-right mr-1"></i>目標：{{ $goals[2]->title }}の詳細はこちら
+    </a>    
     @foreach($efforts[2] as $effort)
     @include('efforts.card')
     @endforeach
