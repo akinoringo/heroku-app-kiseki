be868f359ee12478de72356ad044351a96ee4036
akinoringo <wbraver@outlook.jp>
2021-05-22 23:38:20 +0900

    ec2上でfakerが動作しないためseederからfaker除去

--- backend/database/seeds/EffortSeeder.php
+++ backend/database/seeds/EffortSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Generator as Faker;
 
 class EffortSeeder extends Seeder
 {
@@ -11,7 +10,7 @@ class EffortSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
         //
     	DB::table('efforts')->insert([
@@ -22,7 +21,7 @@ class EffortSeeder extends Seeder
 	    		'title' => 'AWSのEC2にアプリをデプロイした。',
 	    		'content' => 'アプリケーションにPHP-FPM, サーバーにNGINXを用いた。データベースはRDS(MySQL)を用いた。',
 	    		'effort_time' => 2,
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),
     		],
     		[
     			'id' => 2,
@@ -31,7 +30,7 @@ class EffortSeeder extends Seeder
 	    		'title' => 'CircleCIで自動テスト自動デプロイを実装した。',
 	    		'content' => 'CircleCIが便利すぎて、コードデプロイが必要な理由が分からない。',
 	    		'effort_time' => 3,   
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),           			
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),           			
     		],
     		[
     			'id' => 3,
@@ -40,7 +39,7 @@ class EffortSeeder extends Seeder
 	    		'title' => 'CharjsとVue-chartjsを導入した。',
 	    		'content' => 'CharjsとVue-chartjsを用いて積み上げ時間や回数を可視化できるようにした。',
 	    		'effort_time' => 1,    			
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
     		],
         [
           'id' => 4,
@@ -49,7 +48,7 @@ class EffortSeeder extends Seeder
           'title' => '筋トレした。',
           'content' => '朝と夜に30分ずつ筋トレした。',
           'effort_time' => 1,         
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
         ],
         [
           'id' => 5,
@@ -58,7 +57,7 @@ class EffortSeeder extends Seeder
           'title' => '筋トレ',
           'content' => '腕立て/スクワット/腹筋/背筋',
           'effort_time' => 3,         
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
         ],
         [
           'id' => 6,
@@ -67,7 +66,7 @@ class EffortSeeder extends Seeder
           'title' => 'メガネの少年から情報を入手',
           'content' => 'どうやらやつらは黒い服をまとっているらしい。',
           'effort_time' => 2,         
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
         ],         
         [
           'id' => 7,
@@ -76,7 +75,7 @@ class EffortSeeder extends Seeder
           'title' => 'ランニング',
           'content' => '10kmランニング',
           'effort_time' => 2,         
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
         ], 
         [
           'id' => 8,
@@ -85,7 +84,7 @@ class EffortSeeder extends Seeder
           'title' => '名人とカルタ',
           'content' => '本気で2時間ぶっ通した。',
           'effort_time' => 2,         
-          'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
+          // 'created_at' => $faker->dateTimeBetween($startDate = '-3 week', $endDate = '-1 week'),          
         ],                                
   		]); 
   	
