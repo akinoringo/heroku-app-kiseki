8149e7b0b8272d97b885b1a04675b6d506986db3
akinoringo <wbraver@outlook.jp>
2021-05-22 17:16:18 +0900

    目標のフォームリクエストを修正/jsonデコード処理を追記

--- backend/app/Http/Requests/GoalRequest.php
+++ backend/app/Http/Requests/GoalRequest.php
@@ -27,6 +27,7 @@ class GoalRequest extends FormRequest
             //
             'title' => 'required|max:50',
             'content' => 'required|max:500',
+            'tags' => 'json|regex:/^(?!.*\s).+$/u|regex:/^(?!.*\/).*$/u'
             'goal_time' => 'integer|min:10',
             'deadline' => 'required|date|after:today',
         ];
@@ -38,9 +39,23 @@ class GoalRequest extends FormRequest
             //
             'title' => 'タイトル',
             'content' => '本文',
+            'tags' => 'タグ',
             'goal_time' => '目標継続時間',
             'deadline' => '達成期限',
             'today' => '今日'
         ];
-    }    
+    } 
+
+    // フォームリクエストのバリデーション成功後に呼び出されるメソッド
+    public function passedValidation()
+    {
+        // JSONで送られてきたフォームデータをデコードしてコレクションとして代入
+        // sliceでタグ数を絞る。
+        // mapでコククション全てを順番に処理。返り値はコレクション
+        $this->tags = collect(json_decode($this->tags)) 
+            ->slice(0, 5)
+            ->map(function ($requestTag) {
+                return $requestTag->text;
+            });
+    }       
 }
